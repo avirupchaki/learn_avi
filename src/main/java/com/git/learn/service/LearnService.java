728d24439869836ba8906cfeb612345661557b1a
Avirup Chaki <avirupchaki@Avirups-MacBook-Air.local>
2023-06-28 16:22:41 +0100

    enabled lazy loading for RestTemplate bean in the LearnService.

--- src/main/java/com/git/learn/service/LearnService.java
+++ src/main/java/com/git/learn/service/LearnService.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -19,6 +20,7 @@ public class LearnService {
     private String TIME_API_URL;
     
     @Autowired
+    @Lazy
     private RestTemplate restTemplate;
 
     public Time getTime(String city) throws Exception
